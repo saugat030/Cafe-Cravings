4eba6df8f70d63c5225b049d1b9e3877ba5fd508
ZuluMumu <saugatdahal030@gmail.com>
2025-01-13 08:23:55 +0545

    added next and previous arrows

---
+++ src/components/FadeImage.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+const FadeImage = (props) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const handleImageLoad = () => {
+    setIsLoaded(true);
+  };
+
+  useEffect(() => {
+    setIsLoaded(false);
+  }, [props.src]);
+
+  return (
+    <figure
+      className={`transition-opacity duration-700 flex-1 flex items-center h-full justify-center ${
+        isLoaded ? "opacity-100" : "opacity-0"
+      }`}
+    >
+      <img
+        src={props.src}
+        alt="poster_bg"
+        className="h-[800px] rounded-xl"
+        onLoad={handleImageLoad}
+      />
+    </figure>
+  );
+};
+
+export default FadeImage;
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import NavBar from "../components/NavBar";
 import NavigationMenu from "../components/NavigationMenu";
 import Thumbnail from "../components/Thumbnail";
+import FadeImage from "../components/FadeImage";
 import { useState, useEffect } from "react";
+import { GrCaretPrevious } from "react-icons/gr";
+import { GrCaretNext } from "react-icons/gr";
 const Products = () => {
   const [data, setData] = useState(null);
   const [navigationM, setNavigationM] = useState(false);
   const [id, setID] = useState(Math.floor(Math.random() * 10));
+  const [firstIndex, setFirstIndex] = useState(0);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -26,6 +31,7 @@ const Products = () => {
 
   if (data) {
     const item = data.find((item) => item.id === id);
+    let lastIndex = (firstIndex + 3) % data.length;
     return (
       <section>
         <div className="relative z-10 font-Poppins max-h-screen overflow-x-hidden">
@@ -53,10 +59,13 @@ const Products = () => {
                   More
                 </button>
               </div>
-              <div id="best_selling" className="flex flex-col gap-2">
+              <div
+                id="best_selling"
+                className="flex flex-col items-start gap-2"
+              >
                 <h1 className="text-2xl font-semibold">Best Selling</h1>
-                <div className="flex flex-wrap gap-4">
-                  {data.slice(0, 11).map((items) => (
+                <div className="relative flex flex-wrap gap-4 w-auto">
+                  {data.slice(firstIndex, lastIndex + 1).map((items) => (
                     <Thumbnail
                       setID={setID}
                       key={items.id}
@@ -65,18 +74,30 @@ const Products = () => {
                       name={items.name}
                     />
                   ))}
+                  <button
+                    onClick={() => {
+                      if (firstIndex == 0) {
+                        return;
+                      } else {
+                        setFirstIndex(firstIndex - 4);
+                      }
+                    }}
+                    className="absolute top-0 bottom-0 -left-10 z-10 hover:bg-secondary/30 p-2 rounded-xl transition-colors duration-300 transform ease-linear"
+                  >
+                    <GrCaretPrevious className="text-2xl text-black" />
+                  </button>
+                  <button
+                    onClick={() => {
+                      setFirstIndex((firstIndex + 4) % data.length);
+                    }}
+                    className="absolute z-10 top-0 -right-10 bottom-0 hover:bg-secondary/30 p-2 rounded-xl transition-colors duration-300 transform ease-linear"
+                  >
+                    <GrCaretNext className="text-2xl" />
+                  </button>
                 </div>
               </div>
             </div>
-            <figure
-              className={`transition-opacity duration-700 flex-1 flex items-center h-full justify-center`}
-            >
-              <img
-                src={item.poster_url}
-                alt="poster_bg"
-                className="h-[800px] rounded-xl"
-              />
-            </figure>
+            <FadeImage src={item.poster_url} />
           </div>
         </div>
       </section>
